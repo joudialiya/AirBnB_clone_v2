7c0f2e5223de6db98ff5f26123a1dab685109686
Lagzal <mlagzal.sayhi@gmail.com>
2024-07-10 17:36:04 +0000

    1.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this module is a fabric config"""
+
+import os
+import datetime
+from fabric.api import run, local
+
+
+def do_pack():
+    """We pack the web_static content"""
+    dt = datetime.datetime.now(datetime.UTC)
+    archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year,
+        dt.month,
+        dt.day,
+        dt.hour,
+        dt.minute,
+        dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(archive_name)).failed is True:
+        return None
+    return archive_name
