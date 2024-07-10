739da2263dcd68b0f678d16027df7087dbd4e4c0
Lagzal <mlagzal.sayhi@gmail.com>
2024-07-10 18:54:13 +0000

    0.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # This script will setup the server for deployment
 
-apt-get update
-apt-get -y install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
 
 DATA_FOLDER="/data"
 WEB_STATIC="${DATA_FOLDER}/web_static"
@@ -11,24 +11,11 @@ WEB_STATIC_RELEASES="${WEB_STATIC}/releases"
 WEB_STATIC_TEST="${WEB_STATIC_RELEASES}/test"
 WEB_STATIC_CURRENT="${WEB_STATIC}/current"
 
-if [[ ! -d ${WEB_STATIC_TEST} ]];
-then
-    mkdir -p ${WEB_STATIC_TEST}
-fi
-if [[ ! -d ${WEB_STATIC_SHARED} ]];
-then
-    mkdir -p ${WEB_STATIC_SHARED}
-fi
+mkdir -p ${WEB_STATIC_TEST}
+mkdir -p ${WEB_STATIC_SHARED}
 
 # we create th dummy file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+echo "
 <html>
   <head>
   </head>
