3bcf32afacb3928598977bba1cfee6b08929647f
Lagzal <mlagzal.sayhi@gmail.com>
2024-07-10 21:07:59 +0000

    3.

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+"""
+
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+
+
+env.hosts = ['100.26.236.180', '100.25.163.248']
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        dt = datetime.datetime.utcnow()
+        archive_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            dt.year,
+            dt.month,
+            dt.day,
+            dt.hour,
+            dt.minute,
+            dt.second)
+        if not isdir("versions"):
+            local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
+    except Exception as e:
+        print(e)
+        return None
+
+
+def do_deploy(archive_path):
+    """deployment logic"""
+    if not exists(archive_path):
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def deploy():
+    """deploy the archive to the servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
