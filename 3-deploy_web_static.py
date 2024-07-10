f6c8b54c35dc334bd28cf44beb424096ba0b97d8
Lagzal <mlagzal.sayhi@gmail.com>
2024-07-10 21:51:37 +0000

    3.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -33,21 +33,29 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """deployment logic"""
-    if not exists(archive_path):
+    """ deploy the zip in the """
+    if exists(archive_path) is False:
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        return True
-    except Exception as e:
-        print(e)
-        return False
+    else:
+        try:
+            put(archive_path, "/tmp/")
+            basename = os.path.basename(archive_path)
+            basename_no_ext = basename.split(".")[0]
+            target_location = "/data/web_static/releases/"
+            target_location += basename_no_ext
+            target_location += "/"
+            run("mkdir -p " + target_location)
+            run("tar -xzf /tmp/" + basename + " -C " + target_location)
+            run("rm /tmp/" + basename)
+            run("mv " + target_location + "web_static/* " + target_location)
+            run("rm -rf " + target_location + "web_static")
+            run("rm -rf /data/web_static/current")
+            run("ln -s " + target_location + " /data/web_static/current")
+            print("New version deployed!")
+            return True
+        except Exception as e:
+            print("Error:", e)
+            return False
 
 
 def deploy():
