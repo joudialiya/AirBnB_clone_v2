a39f6b980e902a95490811e0e48f5666d183511d
Lagzal <mlagzal.sayhi@gmail.com>
2024-08-02 21:09:05 +0000

    2.

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -22,7 +22,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     '''Route handler'''
-    return "C {}".format(text)
+    return "C {}".format(text).replace("_", " ")
 
 
 if __name__ == '__main__':
